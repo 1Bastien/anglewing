c23e6d3f3ae7dc73b59495e9fedb9c1cc6e6c4bc
Bastien <bgt.bastien@protonmail.com>
2025-05-05 12:36:06 +0200

    feat(Home): Enhance Home component to support animation selection and playback with AnimationPlayer integration

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import styles from "./Home.module.css";
 import Button from "../Button/Button";
 import CloseButton from "../CloseButton/CloseButton";
+import AnimationPlayer from "../AnimationPlayer/AnimationPlayer";
 
 const Home: React.FC = () => {
   const [backgroundStyle, setBackgroundStyle] = useState({});
+  const [selectedAnimation, setSelectedAnimation] = useState<string | null>(
+    null
+  );
 
   useEffect(() => {
     // Utiliser l'image depuis le dossier public/backgrounds
@@ -18,15 +22,30 @@ const Home: React.FC = () => {
     });
   }, []);
 
+  const handleAnimationSelect = (animationNumber: number) => {
+    setSelectedAnimation(`/animations/Animation_${animationNumber}.mp4`);
+  };
+
+  const closeAnimationPlayer = () => {
+    setSelectedAnimation(null);
+  };
+
   return (
     <div className={styles.container}>
       <CloseButton />
       <div className={styles.backgroundImage} style={backgroundStyle}></div>
       <div className={styles.buttonsContainer}>
-        <Button text="Animation 1" />
-        <Button text="Animation 2" />
-        <Button text="Animation 3" />
+        <Button text="Animation 1" onClick={() => handleAnimationSelect(1)} />
+        <Button text="Animation 2" onClick={() => handleAnimationSelect(2)} />
+        <Button text="Animation 3" onClick={() => handleAnimationSelect(3)} />
       </div>
+
+      {selectedAnimation && (
+        <AnimationPlayer
+          animationUrl={selectedAnimation}
+          onClose={closeAnimationPlayer}
+        />
+      )}
     </div>
   );
 };
