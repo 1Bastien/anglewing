f6c5c54d53dc13daabf583ce8af8117f6cde86a3
Bastien <b.bourgeat@docaret.com>
2025-05-14 09:34:36 +0200

    style(closeButton): Revamp CloseButton component with new styles and SVG icon for improved aesthetics and user interaction

--- src/components/CloseButton/CloseButton.tsx
+++ src/components/CloseButton/CloseButton.tsx
@@ -12,7 +12,18 @@ const CloseButton: React.FC = () => {
         onClick={() => setShowModal(true)}
         aria-label="Close application"
       >
-        <span className={styles.hidden}>Close</span>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6.7 5.3a1 1 0 0 0-1.4 1.4L10.6 12l-5.3 5.3a1 1 0 0 0 1.4 1.4L12 13.4l5.3 5.3a1 1 0 0 0 1.4-1.4L13.4 12l5.3-5.3a1 1 0 0 0-1.4-1.4L12 10.6 6.7 5.3z"
+            fill="white"
+          />
+        </svg>
       </button>
       {showModal && <ActionModal onClose={() => setShowModal(false)} />}
     </>
